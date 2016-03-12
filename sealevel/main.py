27da2f970654b5182b58aa89a8aa2c42a61dffb9
Emil Stenström <em@kth.se>
2016-03-12 20:07:32 +0100

    Add boundary checking and debug printout.

--- sealevel/main.py
+++ sealevel/main.py
@@ -1,7 +1,16 @@
 from tempfile import NamedTemporaryFile
-from bottle import route, request, view, response
+from collections import namedtuple
+from bottle import route, request, view, response, abort
 from PIL import Image
 import numpy as np
+
+# Load data file into memory when webserver starts
+INFILE = "elevation_data.npz"
+# ELEVATION_DATA = np.load(INFILE)["arr_0"]
+
+LatLon = namedtuple("LatLon", ["lat", "lon"])
+ELEVATION_TOPLEFT = LatLon(7.3987178, 69.4092519)
+ELEVATION_BOTTOMRIGHT = LatLon(27.5472122, 54.7969333)
 
 @route('/')
 @view('sealevel/views/index')
@@ -11,10 +20,37 @@ def index():
 
 @route('/tile/')
 def tile():
-    tile = NamedTemporaryFile(mode='w+b', suffix='png')
+    nw_lat = float(request.query.nw_lat)
+    nw_lng = float(request.query.nw_lng)
+    se_lat = float(request.query.se_lat)
+    se_lng = float(request.query.se_lng)
 
-    data = np.random.rand(500, 500)
-    data = (data * 255).round(decimals=0)
+    if not (ELEVATION_TOPLEFT.lat < nw_lat < ELEVATION_BOTTOMRIGHT.lat):
+        abort(404, "Nw_lat out of bounds")
+
+    if not (ELEVATION_BOTTOMRIGHT.lon < nw_lng < ELEVATION_TOPLEFT.lon):
+        abort(404, "Nw_lng out of bounds")
+
+    if not (ELEVATION_TOPLEFT.lat < se_lat < ELEVATION_BOTTOMRIGHT.lat):
+        abort(404, "Se_lat out of bounds")
+
+    if not (ELEVATION_BOTTOMRIGHT.lon < se_lng < ELEVATION_TOPLEFT.lon):
+        abort(404, "Se_lng out of bounds")
+
+    assert False, (nw_lat, nw_lng, se_lat, se_lng)
+
+    tile = NamedTemporaryFile(mode='w+b')
+
+    nrows, ncols = 256, 256
+    startrow, startcol = 0, 0
+    max_rows, max_cols = len(ELEVATION_DATA), len(ELEVATION_DATA[0])
+
+    row_from = min(startrow, max_rows)
+    row_to = min(startrow + nrows, max_rows)
+    col_from = min(startcol, max_cols)
+    col_to = min(startcol + ncols, max_cols)
+    data = ELEVATION_DATA[row_from:row_to, col_from:col_to]
+    # assert False, data
 
     image = Image.fromarray(data)
     image = image.convert("RGB")
